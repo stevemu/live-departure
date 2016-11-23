3dd0f82d7fb62e13a7f0a42adef845aceeb9f29e
stevemu <steve.mu.dev@gmail.com>
2016-11-23 12:38:58 -0500

    add loading text

--- src/app/components/FilteredTable.js
+++ src/app/components/FilteredTable.js
@@ -15,49 +15,55 @@ const TextCell = ({rowIndex, data, col, ...props}) => (
 
 let FilteredTable = ({filteredDataList}) => {
   return (
-    <Table
-      rowHeight={50}
-      rowsCount={filteredDataList ? filteredDataList.length : 0}
-      width={680}
-      maxHeight={2000}
-      headerHeight={50}>
-      <Column
-        header={<Cell>Origin</Cell>}
-        cell={<TextCell data={filteredDataList} col="origin"/>}
-        fixed={true}
-        width={120}
-      />
-      <Column
-        header={<Cell>Train#</Cell>}
-        cell={<TextCell data={filteredDataList} col="train"/>}
-        fixed={true}
-        width={80}
-      />
-      <Column
-        header={<Cell>Destination</Cell>}
-        cell={<TextCell data={filteredDataList} col="destination"/>}
-        fixed={true}
-        width={160}
-      />
-      <Column
-        header={<Cell>Depature Time</Cell>}
-        cell={<TextCell data={filteredDataList} col="departureTime"/>}
-        fixed={true}
-        width={140}
-      />
-      <Column
-        header={<Cell>Track#</Cell>}
-        cell={<TextCell data={filteredDataList} col="track"/>}
-        fixed={true}
-        width={60}
-      />
-      <Column
-        header={<Cell>Status</Cell>}
-        cell={<TextCell data={filteredDataList} col="status"/>}
-        fixed={true}
-        width={120}
-      />
-    </Table>
+    <div>
+      {filteredDataList.length > 0 ?
+      <Table
+        rowHeight={50}
+        rowsCount={filteredDataList ? filteredDataList.length : 0}
+        width={680}
+        maxHeight={2000}
+        headerHeight={50}>
+        <Column
+          header={<Cell>Origin</Cell>}
+          cell={<TextCell data={filteredDataList} col="origin"/>}
+          fixed={true}
+          width={120}
+        />
+        <Column
+          header={<Cell>Train#</Cell>}
+          cell={<TextCell data={filteredDataList} col="train"/>}
+          fixed={true}
+          width={80}
+        />
+        <Column
+          header={<Cell>Destination</Cell>}
+          cell={<TextCell data={filteredDataList} col="destination"/>}
+          fixed={true}
+          width={160}
+        />
+        <Column
+          header={<Cell>Depature Time</Cell>}
+          cell={<TextCell data={filteredDataList} col="departureTime"/>}
+          fixed={true}
+          width={140}
+        />
+        <Column
+          header={<Cell>Track#</Cell>}
+          cell={<TextCell data={filteredDataList} col="track"/>}
+          fixed={true}
+          width={60}
+        />
+        <Column
+          header={<Cell>Status</Cell>}
+          cell={<TextCell data={filteredDataList} col="status"/>}
+          fixed={true}
+          width={120}
+        />
+      </Table> :
+        <h3 className="loading">Loading</h3>
+      }
+    </div>
+
   )
 };
 
